e54b5408d5e1ac7c89f68ebdb6c4b8d3e85b025a
Charles Ngo <charlesngo37@gmail.com>
2022-04-18 17:16:50 -0700

    Added comments to the express_server.js. Removed test hello page

--- express_server.js
+++ express_server.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
+// Database of urls and shortened urls.
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
   "9sm5xK" : "http://www.google.com"
 };
 
+// Function to generate a random 6 alphanumeric string.
 const generateRandomString = () => {
   let output = '';
   let characterSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
@@ -19,41 +21,37 @@ const generateRandomString = () => {
   return output;
 };
 
+// Setting EJS as the template engine.
 app.set("view engine", "ejs");
-
+// Middleware to debug connections and parse the buffer when performing post requests.
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+// Home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
-
+// URL database page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Create a new URL to be shortened
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Create pages for the shortURLs in the database
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// Output a .json containing all urls and shortURLS.
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.post("/urls", (req, res) => {
