8a83921126dc20af6800d1c70f3ce04b3ae7fa97
Charles Ngo <charlesngo37@gmail.com>
2022-04-19 21:41:20 -0700

    added ability for users to register and changed the header to show the users email isntead of username

--- express_server.js
+++ express_server.js
@@ -11,6 +11,20 @@ const urlDatabase = {
   "9sm5xK" : "http://www.google.com"
 };
 
+// User database.
+const users = {
+  "randOne": {
+    id: "randOne",
+    email: "a@a.com",
+    password: "123"
+  },
+  "randTwo": {
+    id: "randTwo",
+    email: "s@s.com",
+    password: "123"
+  }
+};
+
 // Function to generate a random 6 alphanumeric string.
 const generateRandomString = () => {
   let output = '';
@@ -31,11 +45,15 @@ app.use(cookieParser());
 
 // Home page
 app.get("/", (req, res) => {
+  res.cookie("user_id", users['randOne'].id);
   res.send("Hello!");
 });
 
+// New user registration page
 app.get("/register", (req, res) => {
   const templateVars = {
+    userId: req.cookies.user_id,
+    userEmail: users[req.cookies.user_id]["email"],
     username: req.cookies["username"],
   };
   res.render("user_register", templateVars);
@@ -44,6 +62,8 @@ app.get("/register", (req, res) => {
 // URL database page
 app.get("/urls", (req, res) => {
   const templateVars = {
+    userId: req.cookies.user_id,
+    userEmail: users[req.cookies.user_id]["email"],
     username: req.cookies["username"],
     urls: urlDatabase
   };
@@ -53,6 +73,8 @@ app.get("/urls", (req, res) => {
 // Create a new URL to be shortened
 app.get("/urls/new", (req, res) => {
   const templateVars = {
+    userId: req.cookies.user_id,
+    userEmail: users[req.cookies.user_id]["email"],
     username: req.cookies["username"],
   };
   res.render("urls_new", templateVars);
@@ -61,12 +83,15 @@ app.get("/urls/new", (req, res) => {
 // Create pages for the shortURLs in the database
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
+    userId: req.cookies.user_id,
+    userEmail: users[req.cookies.user_id]["email"],
     username: req.cookies["username"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// Short URL redirect link
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
@@ -77,43 +102,61 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
+// Adding new short URLs to the database.
 app.post("/urls", (req, res) => {
   const newShortUrl = generateRandomString();
   urlDatabase[newShortUrl] = req.body.longURL;
   res.redirect(`/urls/${newShortUrl}`);
 });
 
+// Removing short URLs from the database
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   const templateVars = {
+    userId: req.cookies.user_id,
+    userEmail: users[req.cookies.user_id]["email"],
     username: req.cookies["username"],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
+// Modifying short URLs
 app.post("/urls/:shortURL/edit", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   const templateVars = {
+    userId: req.cookies.user_id,
+    userEmail: users[req.cookies.user_id]["email"],
     username: req.cookies["username"],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
+// Log in requests
 app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+app.post("/register", (req, res) => {
+  const userId = generateRandomString();
+  users[userId] = {
+    id: userId,
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.cookie("user_id", userId);
   res.redirect("/urls");
 });
 
+// Log out requests
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
 
-
+// Server initialization
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
